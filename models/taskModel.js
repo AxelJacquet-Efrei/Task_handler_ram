5e27a81a28d58e8945e74756a89bd94f5bebcddc
KalinkaGit <remi.grimault@epitech.eu>
2024-11-20 11:36:30 +0100

    feat: added functions

--- models/taskModel.js
+++ models/taskModel.js
@@ -8,11 +8,51 @@ const getTasks = () => {
     return JSON.parse(data);
 };
 
+const modifyTaskName = (tasks, id, name) => {
+    const task = tasks.find(task => task.id === id);
+    task.name = name;
+}
+
+const modifyTaskDescription = (tasks, id, description) => {
+    const task = tasks.find(task => task.id === id);
+    task.description = description;
+}
+
+const modifyTaskStartDate = (tasks, id, startDate) => {
+    const task = tasks.find(task => task.id === id);
+    task.startDate = startDate;
+}
+
+const modifyTaskEndDate = (tasks, id, endDate) => {
+    const task = tasks.find(task => task.id === id);
+    task.endDate = endDate;
+}
+
+const modifyTaskStatus = (tasks, id, status) => {
+    const task = tasks.find(task => task.id === id);
+    task.status = status;
+}
+
+const getTaskById = (tasks, id) => {
+    return tasks.find(task => task.id === id);
+}
+
+const getTasksByName = (tasks, name) => {
+    return tasks.filter(task => task.name === name);
+}
+
 const saveTasks = (tasks) => {
-    fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2), 'utf-8');
-};
+    fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2));
+}
 
 module.exports = {
     getTasks,
     saveTasks,
+    modifyTaskName,
+    modifyTaskDescription,
+    modifyTaskStartDate,
+    modifyTaskEndDate,
+    modifyTaskStatus,
+    getTaskById,
+    getTasksByName
 };
