5544a8c230de92438a512922d60224c26579e5f0
KalinkaGit <remi.grimault@epitech.eu>
2024-11-20 16:19:31 +0100

    fix: usage of array prototypes functions instead of objects

--- models/taskModel.js
+++ models/taskModel.js
@@ -4,46 +4,45 @@ const path = require('path');
 const filePath = path.join(__dirname, '../data/tasks.json');
 
 const getTasks = () => {
-    const data = fs.readFileSync(filePath, 'utf-8');
+    const data = fs.readFileSync(filePath);
     return JSON.parse(data);
 };
 
 const modifyTaskName = (tasks, id, name) => {
-    const task = tasks.find(task => task.id === id);
+    const task = tasks[id];
     task.name = name;
 }
 
 const modifyTaskDescription = (tasks, id, description) => {
-    const task = tasks.find(task => task.id === id);
+    const task = tasks[id];
     task.description = description;
 }
 
 const modifyTaskStartDate = (tasks, id, startDate) => {
-    const task = tasks.find(task => task.id === id);
+    const task = tasks[id];
     task.startDate = startDate;
 }
 
 const modifyTaskEndDate = (tasks, id, endDate) => {
-    const task = tasks.find(task => task.id === id);
+    const task = tasks[id];
     task.endDate = endDate;
 }
 
 const modifyTaskStatus = (tasks, id, status) => {
-    const task = tasks.find(task => task.id === id);
+    const task = tasks[id];
     task.status = status;
 }
 
 const getTaskById = (tasks, id) => {
-    return tasks.find(task => task.id === id);
+    return tasks[id];
 }
 
 const getTasksByName = (tasks, name) => {
-    return tasks.filter(task => task.name === name);
+    return Object.values(tasks).filter(task => task.name === name);
 }
 
 const deleteTask = (tasks, id) => {
-    const index = tasks.findIndex(task => task.id === id);
-    tasks.splice(index, 1);
+    delete tasks[id];
 }
 
 const saveTasks = (tasks) => {
